7bc3cf1b0d7ff478094a3b49060cd27579b6155b
Lance Ito <70550863+Pete803@users.noreply.github.com>
2020-11-27 11:53:38 -0500

    upload

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+# Define the function and have it accept the 'budget_data' as its sole parameter
+def print_percentages(budget_data):
