38da3e26b6a77ba293118e8d358090a3c0ea5e52
Lance Ito <70550863+Pete803@users.noreply.github.com>
2020-11-27 12:10:49 -0500

    commit test UN

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,4 +7,4 @@ budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 # Define the function and have it accept the 'budget_data' as its sole parameter
 def print_percentages(budget_data):
 
-#Comitt test
+#Comitt test USERNAME
