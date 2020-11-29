65bcc91cda0665ca706b61822fa308b1460f0ff4
Lance Ito <70550863+Pete803@users.noreply.github.com>
2020-11-29 13:15:21 -0500

    upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,13 +7,25 @@ import csv
 # then find the average of those changes
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
+
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $38382578
+# Average Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+
 total_months = 0
 net_total = 0
+rev_change = 0
+avg_change = 0
+max_change = 0
+min_change = 0
+
 # Path to collect data from the Resources folder
 budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
-# print(budget_csv)
-# print(type(budget_csv))
-# Read in the CSV file
 with open(budget_csv, 'r') as csvfile:
 
     # Split the data on commas
@@ -23,13 +35,19 @@ with open(budget_csv, 'r') as csvfile:
     # Loop through the data
     for row in csvreader:
         total_months += 1
-        net_total += int(row[1]) 
+        net_total += int(row[1])   
+        avg_change = int(row[1])/len(rev_change)
         print(row)
-        
     
 analysis_report = (
-    f"There are {total_months} total months in this data set\n"
-    f"The net total of data set is ${net_total}\n"
+    "Analysis Report\n"
+    "-------------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Net Total: ${net_total}\n"
+    f"Average Change: ${avg_change}\n"
+    f"Greatest Increase in Profits: ${max_change}\n"
+    f"Greatest Decrease in Profits: ${min_change}\n"
+
 )
 print(analysis_report)
 
