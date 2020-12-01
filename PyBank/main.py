56cd14c93b3a43e98ab63bb479bd3265736ccc43
Lance Ito <70550863+Pete803@users.noreply.github.com>
2020-12-01 12:00:22 -0500

    upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,3 @@
-import os
-import csv
-
 # The total number of months included in the dataset
 # The net total amount of "Profit/Losses" over the entire period
 # Calculate the changes in "Profit/Losses" over the entire period, 
@@ -16,6 +13,9 @@ import csv
 # Greatest Increase in Profits: Feb-2012 ($1926159)
 # Greatest Decrease in Profits: Sep-2013 ($-2196167)
 
+import os
+import csv
+
 
 total_months = 0
 net_total = 0
@@ -25,7 +25,6 @@ min_change = 0
 current_value = 0
 previous_value = 0
 rev_change_list = []
-greatest_inc = 0
 
 # Path to collect data from the Resources folder
 budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
@@ -45,10 +44,10 @@ with open(budget_csv, 'r') as csvfile:
         rev_change = current_value - previous_value
         rev_change_list.append(rev_change)
         previous_value = current_value
-        if greatest_inc < rev_change:
-            greatest_inc = rev_change
-
-        
+        if max_change < rev_change:
+            max_change = rev_change
+        if min_change > rev_change:
+            min_change = rev_change
         print(row)
     
 # Analysis Report    
@@ -58,7 +57,7 @@ analysis_report = (
     f"Total Months: {total_months}\n"
     f"Net Total: ${net_total}\n"
     f"Average Change: ${rev_change}\n"
-    f"Greatest Increase in Profits: {greatest_inc}\n"
+    f"Greatest Increase in Profits: {max_change}\n"
     f"Greatest Decrease in Profits: {min_change}\n"
 
 )
@@ -66,22 +65,3 @@ print(analysis_report)
 
 with open("Analysis_Report.txt",'w') as outputfile:
     outputfile.write(analysis_report)
-
-
-    # [1,3,2,5,4]
-    # max = first ele
-    # loop each ele ----- ele > max ------ max = ele
-    '''
-    max = 1 ele - 1
-    max = 1
-    ele -3 max = 1
-    max = 3
-    ele = 2 max 3
-    max = 3
-    ele = 5 max = 3
-    max = 5
-    ele = 4 max = 5
-    max = 5 
-    
-    
-    '''
